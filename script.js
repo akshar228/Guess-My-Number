71337aa2f8f0bca79b7d0a4b9f1a38c539de64c3
akshar228 <38082528+akshar228@users.noreply.github.com>
2022-03-12 13:49:39 -0500

    NEW CODE

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+function updateScore(score) {
+  document.querySelector('.score').textContent = score;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //when there is no number input
+  if (!guess) {
+    displayMessage('⛔ NO number!');
+
+    //when input is not between 1 and 20
+  } else if (guess <= 0 || guess > 20) {
+    displayMessage('Pick between 1 And 20');
+
+    //when player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    //when number is diffrent then secretnumber
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      updateScore(score);
+    } else {
+      displayMessage('😟 You Lost the Game!');
+      updateScore(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  updateScore(score);
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
