80217ffffe92698942e1c6a89d72fe636e5e0442
akshar228 <38082528+akshar228@users.noreply.github.com>
2022-03-12 14:01:50 -0500

    Minor change

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ document.querySelector('.again').addEventListener('click', function () {
   updateScore(score);
   displayMessage('Start guessing...');
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
+  document.querySelector('.guess').value = ' ';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
